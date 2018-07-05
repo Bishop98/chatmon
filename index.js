03eff2169f878a2c20a4946ba7014509d818712b
mongo3 <32910897+mongo3@users.noreply.github.com>
2018-07-05 16:45:55 -0600

    working cmd log

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
 const puppeteer = require('puppeteer');
 
 
+var loggedIn = false;
+var gPage = "";
 
-let scrape = async () => {
+var last = {user: "", msg: ""};
 
-  const browser = await puppeteer.launch({headless: true});
-  const page = await browser.newPage();
 
-  await page.goto("https://www.reddit.com/chat/r/community_chat/channel/200020_bed753beb7c944a947229ae87a28db8adf2f72a4");
-  await page.waitFor(1000);
-
-  await page.$eval('#loginUsername', el => el.value = "AresPhobos");
-  // await page.click("#loginUsername");
-  await page.$eval('#loginPassword', el => el.value = "");
-  await page.click(".AnimatedForm__submitButton");
-  await page.waitFor(200);
-  await page.keyboard.press("Enter");
-  await page.waitFor(15000);
-
-  const result = await page.evaluate(() => {
-    // document.querySelector("body > ul > li.login.page > div > h3").innerHTML
-    var v = document.querySelector("#tooltip-container > div > div:last-child > div > div > div > div:last-child > div > pre").innerHTML;
-
-    return {
-      v
+var scrape = async () => {
+  if (loggedIn === false) {
+    console.log("waiting for page");
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+    await page.goto("https://www.reddit.com/chat/r/community_chat/channel/200020_bed753beb7c944a947229ae87a28db8adf2f72a4");
+    await page.waitFor(3000);
+    console.log("page loaded");
+    await page.$eval('#loginUsername', el => el.value = "AresPhobos");
+    await page.$eval('#loginPassword', el => el.value = "");
+    await page.click(".AnimatedForm__submitButton");
+    await page.waitFor(200);
+    console.log("logging in");
+    await page.keyboard.press("Enter");
+    await page.waitFor(10000);
+    gPage = page;
+    loggedIn = true;
+    scrape();
+  } else {
+    try {
+      var result = await gPage.evaluate(() => {
+        var user = document.querySelector("#tooltip-container > div > div:last-child > div > div > div > a:last-of-type > h4").innerHTML;
+        var msg = document.querySelector("#tooltip-container > div > div:last-child > div > div > div > div:last-child > div > pre").innerHTML;
+        return {
+          user, msg
+        }
+      });
+    } catch (e) {
+      console.log("WARNING: CHAT STICKERS NOT SUPPOERTED");
     }
-  });
-
-  browser.close();
-  return result;
+    if (result.msg !== last.msg) {
+      console.log(result.user + ": " + result.msg);
+      last = result;
+    }
+  }
 }
 
-scrape().then((value) => {
-  console.log(value);
-});
+scrape();
 
-// });
+setInterval(function(){if (loggedIn) {scrape()} else {} }, 200);
